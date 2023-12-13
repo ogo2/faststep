bb0667a15ef6c159e0e4b588662f933ae810055e
ogo2 <moostral3@yandex.ru>
2023-12-13 23:48:07 +0300

    post

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.exc import IntegrityError
 from pydantic import BaseModel
 import service
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -28,7 +29,20 @@ class ProductSchema(BaseModel):
 @app.get("/product", response_model=List[ProductSchema])
 async def get_biggest_cities(session: AsyncSession = Depends(get_session)):
     products = await service.get_product(session)
-    return [ProductSchema(name=c.name) for c in products]
+    return [ProductSchema(name=c.name, photo_path=c.photo_path, price=c.price, old_price=c.old_price, url_product=c.url_product, sex=c.sex) for c in products]
+# сессия может быть внедрена (инжектирована) с помощью Depends. Таким образом, вызов каждого из маршрутов создаст новую сессию. 
+# Для получения данных единственное изменение заключается в том, что теперь мы хотим применить await для нашей сервисной функции.
+@app.post('/admin/add/')
+async def add_product(product: ProductSchema, session: AsyncSession = Depends(get_session)): 
+    product = service.add_product(session, product.name, product.photo_path, product.price, product.old_price, product.url_product, product.sex)
+    await session.commit()
+    return product
+    
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 
 # @app.get('/admin/add/')
 # async def new_product(product: ProductSchema):
@@ -38,19 +52,3 @@ async def get_biggest_cities(session: AsyncSession = Depends(get_session)):
 #                    old_price=22.1,
 #                    url_product='https://www.ae.com/ca/en/p/women/shoes/sneakers/ae-embroidered-platform-sneaker/0414_6176_256?menu=cat4840004',
 #                    sex='men')
-
-# сессия может быть внедрена (инжектирована) с помощью Depends. Таким образом, вызов каждого из маршрутов создаст новую сессию. 
-# Для получения данных единственное изменение заключается в том, что теперь мы хотим применить await для нашей сервисной функции.
-@app.post('/admin/add/')
-async def add_product(product: ProductSchema, session: AsyncSession = Depends(get_session)): 
-    product = service.add_product(session, product.name, product.photo_path, product.price, product.old_price, product.url_product, product.sex)
-    try:
-        await session.commit()
-        return product
-    except IntegrityError as ex:
-        await session.rollback()
-        raise DuplicatedEntryError("Ошибка")
-    
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
