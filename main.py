34f187dd726422557557ddd6df39c515a9b6fb55
Hyperes <moostral3@yandex.ru>
2023-12-20 18:37:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Form
 from typing import Union
 from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, Request, Query
@@ -13,25 +13,40 @@ from typing import List
 from fastapi import Depends
 from base import get_session
 from fastapi_pagination import Page, add_pagination, paginate
+from typing_extensions import Annotated
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
 
 class ProductSchema(BaseModel):
     name: str
     photo_path: str
-    price: float
-    old_price: float
+    price: int
+    old_price: int
     url_product: str
     sex: str
 
-@app.get("/product", response_model=List[ProductSchema])
-async def get_biggest_cities(session: AsyncSession = Depends(get_session)):
-    products = await service.get_product(session)
-    return [ProductSchema(name=c.name, photo_path=c.photo_path, price=c.price, old_price=c.old_price, url_product=c.url_product, sex=c.sex) for c in products]
+class UserSchema(BaseModel):
+    name: str
+    phone: str
+    email: str
+    password: str
+    product_list: list
+    date_registr: str
+
+
 # сессия может быть внедрена (инжектирована) с помощью Depends. Таким образом, вызов каждого из маршрутов создаст новую сессию. 
 # Для получения данных единственное изменение заключается в том, что теперь мы хотим применить await для нашей сервисной функции.
 @app.post('/admin/add/')
@@ -48,13 +63,6 @@ async def index(request: Request, session: AsyncSession = Depends(get_session)):
 # cтраница с кроссовками
 @app.get('/shoes', response_class=HTMLResponse, response_model=List[ProductSchema])
 async def product_list(request: Request, session: AsyncSession=Depends(get_session), page: int = 1):
-    
-    # if page == 1:
-    #     skip = 0
-    #     limit = 12
-    # elif page>1:
-    #     skip = page * 12
-    #     limit = 12
     dictionary = {}
     limit = 12
     for i in range(1, 100): 
@@ -65,22 +73,26 @@ async def product_list(request: Request, session: AsyncSession=Depends(get_sessi
             
     products = await service.get_product_pag(session, dictionary[page], limit)
     print(dictionary[page])
-
-            
     products_all = await service.get_product_all(session)
     data_brand = await service.get_brand_name(session)
     list_all = len(products_all) / 12
-    # count_brand = await service.get_product_count(session)
     return templates.TemplateResponse("shop.html", {"request": request, 'products': products, 'brand': data_brand, 'product_all': int(list_all), 'dictionary': dictionary, 'page': page})
-
 
 @app.get('/shoes/{name_product}/{id_product}', response_class=HTMLResponse, response_model=List[ProductSchema])
 async def product_page(name_product: str, id_product: int,  request: Request, session: AsyncSession=Depends(get_session)):
     product = await service.get_product_page(session, id_product)
     return templates.TemplateResponse("product-page.html", {"request": request, 'product': product, 'name_product': name_product})
 
-@app.get('/admin', response_class=HTMLResponse)
-async def add_product_page(request: Request):
-    return templates.TemplateResponse("admin.html", {'request': request})
+@app.get('/login', response_class=HTMLResponse)
+async def login(request: Request, session: AsyncSession=Depends(get_session)):
+    return templates.TemplateResponse('login.html', {'request': request})
+
+@app.get('/register', response_class=HTMLResponse)
+async def register(request: Request, session: AsyncSession=Depends(get_session)):
+    return templates.TemplateResponse('register.html', {'request': request})
 
 
+@app.post('/register')
+async def register_user(username: str = Form(), password: str = Form()):
+    hashed_password = hash_password(password)
+    return {"username": username, 'hashed_password': hashed_password}
