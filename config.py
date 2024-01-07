22757b2a50f8cc1ddd0096921c61352d174bfd88
Hyperes <moostral3@yandex.ru>
2024-01-07 21:51:17 +0300

    Delete config.py

--- config.py
+++
@@ -1,2 +0,0 @@
-DB_CONNECT = 'postgresql+asyncpg://isrzeyjx:P4wzfAfwaiJmP_zCdYuvVPp0fcMkBvXl@cornelius.db.elephantsql.com/isrzeyjx'
-SECRET_KEY = "adfpokghfdl$kjvlvzcxkjlhzcxv#jkkjhdfis43iofadifkjdjkzcvx.kzcvxk"
