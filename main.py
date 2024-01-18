6a9bea2c0775eed6cb4ff8ab39ca082530429ec8
Hyperes <moostral3@yandex.ru>
2024-01-18 22:49:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, File, Form, HTTPException, Depends, FastAPI, Request, Query, Cookie, Response, status
+from fastapi import FastAPI, File, Form, HTTPException, Depends, FastAPI, Request, Query, Cookie, Response, status, Security
 from typing import Union
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.exc import IntegrityError
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from pydantic import BaseModel
 import service
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -13,22 +15,33 @@ from base import get_session
 from fastapi_pagination import Page, add_pagination, paginate
 from typing_extensions import Annotated
 import secrets
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
 from token2 import create_jwt_token, verify_jwt_token
 from models import User
+from fastapi.middleware.cors import CORSMiddleware
 from jose import JWTError, jwt
 
 
 app = FastAPI()
-
+security = HTTPBearer()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 templates = Jinja2Templates(directory="templates")
-
+# Добавьте эти настройки, чтобы разрешить запросы от вашего сайта Ajax
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 async def hash_password(password: str):
     return pwd_context.hash(password)
+
+class ProductWish(BaseModel):
+    id_product: Union[int, str]
 
 class ProductSchema(BaseModel):
     name: str
@@ -44,7 +57,7 @@ class UserSchema(BaseModel):
     email: str
     password: str
     activate: Union[str, None] = None
-    remember_me: bool
+    subscribe: bool
     product_list: Union[str, None] = None
     date_registr: Union[str, None] = None
     
@@ -55,32 +68,29 @@ class UserAuth(BaseModel):
     email: str
     password: str
 
-async def get_current_user(jwt_token: str = Cookie(), session: AsyncSession=Depends(get_session)):
+async def get_current_user(request: Request, session: AsyncSession=Depends(get_session)):
     try:
-        decoded_data = verify_jwt_token(jwt_token)
+        decoded_data = verify_jwt_token(request.cookies.get("jwt_token"))
         if not decoded_data:
             return None                 #токен есть но у него закончилось время, пользователь не вышел
         user = await service.get_user_login(session, decoded_data["sub"])  # Получите пользователя из базы данных
         return user
     except Exception:
         return None 
-# сессия может быть внедрена (инжектирована) с помощью Depends. Таким образом, вызов каждого из маршрутов создаст новую сессию. 
-# Для получения данных единственное изменение заключается в том, что теперь мы хотим применить await для нашей сервисной функции.
-@app.post('/admin/add/')
-async def add_product(product: ProductSchema, session: AsyncSession = Depends(get_session)): 
-    product = service.add_product(session, product.name, product.photo_path, product.price, product.old_price, product.url_product, product.sex)
-    await session.commit()
-    return product 
-    
+
 @app.get("/", response_class=HTMLResponse, response_model=List[ProductSchema])
-async def index(request: Request, session: AsyncSession = Depends(get_session)):
+async def index(request: Request, session: AsyncSession = Depends(get_session), 
+                current_user: dict = Depends(get_current_user)):
     products = await service.get_product(session)
-    return templates.TemplateResponse("index.html", {"request": request, 'products': products})
+    products_all = await service.get_product_all(session)
+    return templates.TemplateResponse("index.html", {"request": request, 'products': products, 'products_all': products_all,
+                                                     'current_user': current_user})
 
 # cтраница с кроссовками
 @app.get('/shoes', response_class=HTMLResponse, response_model=List[ProductSchema])
-async def product_list(request: Request, session: AsyncSession=Depends(get_session), page: int = 1, current_user: dict = Depends(get_current_user)):
-    dictionary = {}
+async def product_list(request: Request, session: AsyncSession=Depends(get_session), page: int = 1, 
+                       current_user: dict = Depends(get_current_user)):
+    dictionary = {}             #словарь с кол-вом страниц и кол-вом товара который нужно пропустить  при sql запросе (skip)
     limit = 12
     for i in range(1, 100): 
         if i == 1:
@@ -88,28 +98,65 @@ async def product_list(request: Request, session: AsyncSession=Depends(get_sessi
         else:
             dictionary[i] = dictionary[i-1] + 12
             
-    products = await service.get_product_pag(session, dictionary[page], limit)
-    print(dictionary[page])
-    products_all = await service.get_product_all(session)
+    products = await service.get_product_pag(session, dictionary[page], limit)      #sql запрос который пропускает (skip) и выводит нужное кол-во товара (limit)
+    products_all = await service.get_product_all(session)   
     data_brand = await service.get_brand_name(session)
+    list_all = len(products_all) / 12           #считает кол-во страниц
+    return templates.TemplateResponse("shop.html", {"request": request, 'products': products, 
+                                                    'brand': data_brand, 'product_all': int(list_all), 'products_all': products_all,
+                                                    'dictionary': dictionary, 'page': page, 'current_user': current_user})
+# обработка фильтра на странице с кросовками
+@app.get('/shoes/filtrs', response_class=HTMLResponse, response_model=List[ProductSchema])
+async def product_filtr(request: Request, brand: str = Query(None), price: str = Query(None), 
+                        select_item: str = Query(None), session: AsyncSession=Depends(get_session),  page: int = 1, 
+                        current_user: dict = Depends(get_current_user)):
+
+    if brand != None:
+        brand = brand.split(',')
+    if price != None:
+        price = price.split('-')
+    dictionary = {}
+    limit = 12
+    print({'brand': brand, 'price': price, 'select_item': select_item})
+
+    for i in range(1, 100): 
+        if i == 1:
+            dictionary[i] = 0
+        else:
+            dictionary[i] = dictionary[i-1] + 12
+    
+    print({'page': page})
+    products = await service.get_filter_prod(session, brand=brand, price=price, select_item=select_item, skip=dictionary[page], limit=limit)
+    products_all = await service.get_filter_prod_all(session, brand=brand, price=price, select_item=select_item)
+    data_brand = await service.get_brand_name(session)
+    # print({'prod': len(products_all)})
+    
     list_all = len(products_all) / 12
     return templates.TemplateResponse("shop.html", {"request": request, 'products': products, 
-                                                    'brand': data_brand, 'product_all': int(list_all), 
-                                                    'dictionary': dictionary, 'page': page, 'current_user': current_user})
-
+                                                    'brand': data_brand, 'product_all': int(list_all), 'products_all': products_all,
+                                                    'dictionary': dictionary, 'page': page, 'current_user': current_user, 'price': price,
+                                                    'bool_filtr': True, 'query': request.query_params})
+    
 @app.get('/shoes/{name_product}/{id_product}', response_class=HTMLResponse, response_model=List[ProductSchema])
-async def product_page(name_product: str, id_product: int,  request: Request, session: AsyncSession=Depends(get_session)):
+async def product_page(name_product: str, id_product: int,  request: Request, session: AsyncSession=Depends(get_session), 
+                       current_user: dict = Depends(get_current_user)):
     product = await service.get_product_page(session, id_product)
-    return templates.TemplateResponse("product-page.html", {"request": request, 'product': product, 'name_product': name_product})
+    products_all = await service.get_product_all(session)
+    return templates.TemplateResponse("product-page.html", {"request": request, 'product': product, 
+                                                            'name_product': name_product, 'current_user': current_user, 
+                                                            'products_all': products_all})
 
 @app.get('/login', response_class=HTMLResponse)
-async def login(request: Request, session: AsyncSession=Depends(get_session)):
-    return templates.TemplateResponse('login.html', {'request': request})
+async def login(request: Request, session: AsyncSession=Depends(get_session), current_user: dict = Depends(get_current_user)):
+    if not current_user:
+        return templates.TemplateResponse('login.html', {'request': request})
+    return RedirectResponse(url="/")
 
 @app.get('/register', response_class=HTMLResponse)
-async def register(request: Request, session: AsyncSession=Depends(get_session)):
-    return templates.TemplateResponse('register.html', {'request': request})
-
+async def register(request: Request, session: AsyncSession=Depends(get_session), current_user: dict = Depends(get_current_user)):
+    if not current_user:
+        return templates.TemplateResponse('register.html', {'request': request})
+    return RedirectResponse(url="/")
 
 @app.post('/register', response_model=UserSchema)
 async def register_token(user: UserSchema, session: AsyncSession=Depends(get_session)):
@@ -119,12 +166,12 @@ async def register_token(user: UserSchema, session: AsyncSession=Depends(get_ses
         'password': hashed_password,
         'activate': token
     }
-    user.add_data(data)
+    user.add_data(data)        #добавляем в модель пользователя захэшированный пароль и токен для активации учетной записи
     add_user = await service.register_user(session, name=user.name, phone=user.phone, email=user.email, 
-                                        password=user.password, remember_me=user.remember_me, 
+                                        password=user.password, subscribe=user.subscribe, 
                                         token=user.activate)
     await session.commit()
-    print(token)
+    print(token)    #<-------НУЖНО ДОПИСАТЬ ФУНКЦИЮ ДЛЯ ОТПРАВКИ ПОЛЬЗОВАТЕЛЮ НА ПОЧТУ ТОКЕН ДЛЯ ПОДТВЕРЖДЕНИЯ
     return user
 
 @app.get('/register/{activate}')
@@ -132,7 +179,7 @@ async def finish_register(activate: str,  session: AsyncSession=Depends(get_sess
     user = await service.get_user(session, activate)
     try:
         user = user[0][0]
-        if user.activate == activate:
+        if user.activate == activate:   #сверяем ключ активации
             user = await service.activate_user(session, activate)
             await session.commit()
             return 'Регистрация подтверждена'
@@ -141,29 +188,67 @@ async def finish_register(activate: str,  session: AsyncSession=Depends(get_sess
     except Exception:
         return 'Ошибка 404'
 
-@app.get('/login', response_class=HTMLResponse)
-async def login(request: Request, session: AsyncSession=Depends(get_session)):
-    return templates.TemplateResponse('login.html', {'request': request})
-
+#аутентификация пользователя
 @app.post('/login')
 async def auth_login(response: Response, user: UserAuth, session: AsyncSession=Depends(get_session) ):
-    
-    user_sql = await service.get_user_login(session, user.email)
+    user_sql = await service.get_user_login(session, user.email)    #получаем пользователя из бд для проверки данных 
     user_sql = user_sql[0]
-    if user_sql.activate != 'activate':
+    if user_sql.activate != 'activate':     #проверка подтвердил ли пользователь свой профиль
         return 'Не подтвержденный профиль!!!'
     is_password_correct = pwd_context.verify(user.password, user_sql.password)
     jwt_token = create_jwt_token({"sub": user_sql.email})
-    response.set_cookie(key="jwt_token", value=jwt_token)
+    response.set_cookie(key="jwt_token", value=jwt_token)   #сохраняем токен в куки сайта
     return {"access_token": jwt_token, "token_type": "bearer", 
             'user_id': user_sql.id, 'email': user_sql.email }
-
-@app.get('/logout')
-async def logout(response: Response):
+# Выход
+@app.post('/logout')
+async def logout(response: Response, current_user: dict = Depends(get_current_user)):
+    if not current_user:
+        return RedirectResponse(url="/")
     response.delete_cookie("jwt_token")
-    return response, RedirectResponse(url="/shoes")
+    return {'del': 'del'}
+# добавление в избранное товар
+@app.post("/add/product/wishlist/")
+async def add_product_wishlist(product_wish: ProductWish, current_user: dict = Depends(get_current_user),
+                               session: AsyncSession=Depends(get_session)):
+    user = current_user[0]
+    wish = []
+    if user.product_list == None:
+        wish.append(int(product_wish.id_product))
+        await service.add_wish(session, user.email, wish)
+        await session.commit()
+        return wish
+    for i in user.product_list:
+        if i == int(product_wish.id_product):
+            return None
+    info_product = await service.get_product_page(session, int(product_wish.id_product))
+    user.product_list.append(int(product_wish.id_product))
+    await service.add_wish(session, user.email, user.product_list)
+    await session.commit()
+    return {'product_list': user.product_list, 'info_product': info_product}
+# удаление из избранного
+@app.post("/delete/product/wishlist/")
+async def delete_product_wishlist(product_wish: ProductWish, current_user: dict = Depends(get_current_user),
+                               session: AsyncSession=Depends(get_session)):
+    user = current_user[0]
+    user.product_list.remove(int(product_wish.id_product))
+    await service.add_wish(session, user.email, user.product_list)
+    await session.commit()
+    return user.product_list
 
+@app.get('/wishlist', response_class=HTMLResponse)
+async def login(request: Request, session: AsyncSession=Depends(get_session), 
+                current_user: dict = Depends(get_current_user)):
+    products_all = await service.get_product_all(session)
+    return templates.TemplateResponse('wishlist.html', {'request': request, 
+                                                        'current_user': current_user,
+                                                        'products_all': products_all})
 
-@app.get("/users/me")
-async def get_user_me(current_user: dict = Depends(get_current_user)):
-    return current_user
+@app.post("/add/sub/")
+async def subscribe(current_user: dict = Depends(get_current_user),
+                    session: AsyncSession=Depends(get_session)):
+    
+    user = current_user[0]
+    await service.subscribe_me_up(session, user.email)
+    await session.commit()
+    return 123
--- service.py
+++ service.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from models import *
-from typing import List
+from typing import List, Union
 from sqlalchemy import func
 from datetime import datetime
 
@@ -32,27 +32,36 @@ async def get_user_login(session: AsyncSession, email: str) -> List['User']:
     return result.scalars().all()
 
 async def register_user(session: AsyncSession, name: str, phone: str, 
-                        email: str, password: str, remember_me: bool, token: str)->List['User']:
+                        email: str, password: str, subscribe: bool, token: str)->List['User']:
     new_user = User(name=name,
                     phone=phone,
                     email=email,
                     password=password,
-                    remember_me=remember_me,
+                    subscribe=subscribe,
                     date_registr=datetime.now(),
                     activate=token)
     session.add(new_user)
     return new_user
 
+
 async def activate_user(session: AsyncSession, activate: str):
     activate_user = await session.execute(update(User).where(User.activate == activate).values(activate='activate'))
     return activate_user
+
+async def subscribe_me_up(session: AsyncSession, email: str):
+    subscribe_user = await session.execute(update(User).where(User.email == email).values(subscribe=True))
+    return subscribe_user
 
 async def add_token(session: AsyncSession, token: str, email: str):
     add_token = await session.execute(update(User).where(User.email==email).values(token=token))
     return add_token
 
+async def add_wish(session: AsyncSession, email: str, product_wish: dict):
+    add_wish = await session.execute(update(User).where(User.email==email).values(product_list=product_wish))
+    return add_wish
+
 async def get_brand_name(session: AsyncSession) -> List['Product']:
-    result = await session.execute(select(Product.brand, func.count(Product.brand).label("brand_count")).group_by(Product.brand).order_by(func.count(Product.brand).desc()))
+    result = await session.execute(select(func.lower(Product.brand), func.count(func.lower(Product.brand)).label("brand_count")).group_by(func.lower(Product.brand)).order_by(func.count(func.lower(Product.brand)).desc()))
     data = result.all()
     for i in range(len(data)):
         data[i] = list(data[i])
@@ -60,6 +69,84 @@ async def get_brand_name(session: AsyncSession) -> List['Product']:
             data[i][0] = data[i][0].strip("''")
     return data
 
+# фильтры
+async def get_filter_prod(session: AsyncSession, skip, limit, select_item: str, brand: Union[dict, None] = None, 
+                          price: Union[dict, None] = None):
+    if select_item == 'new':
+        if brand and price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1])), func.lower(Product.brand).in_(brand)).order_by(Product.date.desc()).offset(skip).limit(limit))
+            return result.scalars().all()
+        if brand:
+            result = await session.execute(select(Product).where(func.lower(Product.brand).in_(brand)).order_by(Product.date.desc()).offset(skip).limit(limit))
+            return result.scalars().all()
+        if price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1]))).order_by(Product.date.desc()).offset(skip).limit(limit)) 
+            return result.scalars().all()
+        result = await session.execute(select(Product).order_by(Product.date.desc()).offset(skip).limit(limit)) 
+        return result.scalars().all()
+    if select_item == 'min':
+        if brand and price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1])), func.lower(Product.brand).in_(brand)).order_by(Product.price.asc()).offset(skip).limit(limit))
+            return result.scalars().all()
+        if brand:
+            result = await session.execute(select(Product).where(func.lower(Product.brand).in_(brand)).order_by(Product.price.asc()).offset(skip).limit(limit))
+            return result.scalars().all()
+        if price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1]))).order_by(Product.price.asc()).offset(skip).limit(limit)) 
+            return result.scalars().all()
+        result = await session.execute(select(Product).order_by(Product.price.asc()).offset(skip).limit(limit)) 
+        return result.scalars().all()
+    if select_item == 'max':
+        if brand and price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1])), func.lower(Product.brand).in_(brand)).order_by(Product.price.desc()).offset(skip).limit(limit)) 
+            return result.scalars().all()
+        if brand:
+            result = await session.execute(select(Product).where(func.lower(Product.brand).in_(brand)).order_by(Product.price.desc()).offset(skip).limit(limit))
+            return result.scalars().all()
+        if price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1]))).order_by(Product.price.desc()).offset(skip).limit(limit)) 
+            return result.scalars().all()
+        result = await session.execute(select(Product).order_by(Product.price.desc()).offset(skip).limit(limit)) 
+        return result.scalars().all()
+    
+async def get_filter_prod_all(session: AsyncSession, select_item: str, brand: Union[dict, None] = None, 
+                          price: Union[dict, None] = None):
+    if select_item == 'new':
+        if brand and price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1])), func.lower(Product.brand).in_(brand)).order_by(Product.date.desc()))
+            return result.scalars().all()
+        if brand:
+            result = await session.execute(select(Product).where(func.lower(Product.brand).in_(brand)).order_by(Product.date.desc()))
+            return result.scalars().all()
+        if price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1]))).order_by(Product.date.desc())) 
+            return result.scalars().all()
+        result = await session.execute(select(Product).order_by(Product.date.desc())) 
+        return result.scalars().all()
+    if select_item == 'min':
+        if brand and price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1])), func.lower(Product.brand).in_(brand)).order_by(Product.price.asc()))
+            return result.scalars().all()
+        if brand:
+            result = await session.execute(select(Product).where(func.lower(Product.brand).in_(brand)).order_by(Product.price.asc()))
+            return result.scalars().all()
+        if price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1]))).order_by(Product.price.asc())) 
+            return result.scalars().all()
+        result = await session.execute(select(Product).order_by(Product.price.asc())) 
+        return result.scalars().all()
+    if select_item == 'max':
+        if brand and price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1])), func.lower(Product.brand).in_(brand)).order_by(Product.price.desc())) 
+            return result.scalars().all()
+        if brand:
+            result = await session.execute(select(Product).where(func.lower(Product.brand).in_(brand)).order_by(Product.price.desc()))
+            return result.scalars().all()
+        if price:
+            result = await session.execute(select(Product).where(Product.price.between(int(price[0]), int(price[1]))).order_by(Product.price.desc())) 
+            return result.scalars().all()
+        result = await session.execute(select(Product).order_by(Product.price.desc())) 
+        return result.scalars().all()
 # Функция add_product просто помещает новый объект Product в сессию — мы будем управлять транзакцией в контроллере (маршрут).
 def add_product(session: AsyncSession, 
              name: str, photo_path: str, price: float,
